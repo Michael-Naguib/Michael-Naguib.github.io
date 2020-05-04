7f7d28bd70d80baa584795868dfe1e267046ee58
Michael Naguib <1michael.naguib@gmail.com>
2020-05-04 11:41:22 -0500

    Updates

---
+++ precache-manifest.7556b811d98a0385597f984a25b0d6e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "259555168899a2ad92a84d04a03c8fb0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09d220e2de9a723fa09b",
+    "url": "/static/css/main.45ce17b1.chunk.css"
+  },
+  {
+    "revision": "90909109f4bbabfefd49",
+    "url": "/static/js/2.812f093b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.812f093b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09d220e2de9a723fa09b",
+    "url": "/static/js/main.342709e7.chunk.js"
+  },
+  {
+    "revision": "447edeee62eb2871bcd0",
+    "url": "/static/js/runtime-main.c9a36f42.js"
+  }
+]);
