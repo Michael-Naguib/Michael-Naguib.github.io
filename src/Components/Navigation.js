bf5323f72107ca44b926455f83466a0c2cbd7c19
Michael Naguib <1michael.naguib@gmail.com>
2021-03-31 15:12:22 -0500

    Updating resume on site

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Navbar} from 'react-bootstrap';//,NavItem
 import Nav from 'react-bootstrap/Nav';
 import { HashLink as Link } from 'react-router-hash-link';
+import "./Navigation.scss";
 //import { LinkContainer } from 'react-router-bootstrap';
 //import { NavLink,Link } from 'react-router-dom'; // code works without it... thanks https://reactjsexample.com/hash-link-scroll-functionality-for-react-router/
 
@@ -14,15 +15,15 @@ function Navigation(props) {
 
     //Hedgehog home link will not work in dev ... will work in production ...
     return(
-        <Navbar expand="md" className="sticky-top" sticky="top" bg={mode} variant={mode} style={{top:"0",position:"fixed",width:"100%"}} >
+        <Navbar expand="md" className="sticky-top" sticky="top" bg={mode} variant={mode} style={{top:"0",position:"fixed",width:"100%",}} >
             <Navbar.Brand><Link className={"nav-link"} to={"/"}> 🦔</Link></Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
-                    <Link className={"nav-link"} to={"/#AboutMe"}> About Me</Link>
-                    <Link className={"nav-link"} to={"/#Projects"}> Projects</Link>
-                    <Link className={"nav-link"} to={"/#Contact"}> Contact</Link>
-                    <Link className={"nav-link"} to={"/Demos"}> Demos</Link>
+            <Navbar.Collapse id="basic-navbar-nav" >
+                <Nav className="mr-auto" >
+                    <Link className={"nav-link "} to={"/#AboutMe"}> About Me</Link>
+                    <Link className={"nav-link "} to={"/#Projects"}> Projects</Link>
+                    <Link className={"nav-link "} to={"/#Contact"}> Contact</Link>
+                    <Link className={"nav-link "} to={"/Demos"}> Demos</Link>
                     <Link className={"nav-link"} to={"/Articles"}> Articles</Link>
                 </Nav>
             </Navbar.Collapse>
